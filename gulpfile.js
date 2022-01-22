346a642d1ccc2c8146a3f5cd23972af726da03fd
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2022-01-22 18:58:21 +0530

    Project Completed :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-const gulp = require("gulp");
-const { parallel, series } = require("gulp");
+const gulp = require('gulp');
+const { parallel, series } = require('gulp');
 
-const imagemin = require("gulp-imagemin");
-const htmlmin = require("gulp-htmlmin");
-const uglify = require("gulp-uglify");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
-const concat = require("gulp-concat");
-const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp#page-top
-const nunjucksRender = require("gulp-nunjucks-render");
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create(); //https://browsersync.io/docs/gulp#page-top
+const nunjucksRender = require('gulp-nunjucks-render');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
-const font2css = require("gulp-font2css").default;
+const font2css = require('gulp-font2css').default;
 
 // /*
 // TOP LEVEL FUNCTIONS
@@ -22,110 +22,116 @@ const font2css = require("gulp-font2css").default;
 
 // Optimise Images
 function imageMin(cb) {
-    gulp.src("src/assets/images/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/images"));
-    cb();
+  gulp
+    .src('src/assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+  cb();
 }
 
 // Copy all HTML files to Dist
 function copyHTML(cb) {
-    gulp.src("src/*.html").pipe(gulp.dest("dist"));
-    cb();
+  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+  cb();
 }
 
 // Minify HTML
 function minifyHTML(cb) {
-    gulp.src("src/*.html")
-        .pipe(gulp.dest("dist"))
-        .pipe(
-            htmlmin({
-                collapseWhitespace: true
-            })
-        )
-        .pipe(gulp.dest("dist"));
-    cb();
+  gulp
+    .src('src/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest('dist'));
+  cb();
 }
 
 // Scripts
 function js(cb) {
-    gulp.src("src/assets/js/*js")
-        .pipe(babel({
-            presets: ['@babel/preset-env']
-        }))
-        .pipe(concat("main.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/js"));
-    cb();
+  gulp
+    .src('src/assets/js/*js')
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+  cb();
 }
 
 // Compile Sass
 function css(cb) {
-    gulp.src("src/assets/sass/*.scss")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(autoprefixer({
-            browserlist: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("dist/css"))
-        // Stream changes to all browsers
-        .pipe(browserSync.stream());
-    cb();
+  gulp
+    .src('src/assets/sass/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browserlist: ['last 2 versions'],
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest('dist/css'))
+    // Stream changes to all browsers
+    .pipe(browserSync.stream());
+  cb();
 }
 
 // Fonts
 function fonts(cb) {
-    gulp
-      .src("src/static/fonts/**/*.{otf,ttf,woff,woff2}")
-      .pipe(font2css())
-      .pipe(concat("fonts.css"))
-      .pipe(gulp.dest("dist/"));
-    cb();
-  }
-
+  gulp
+    .src('src/static/fonts/**/*.{otf,ttf,woff,woff2}')
+    .pipe(font2css())
+    .pipe(concat('fonts.css'))
+    .pipe(gulp.dest('dist/'));
+  cb();
+}
 
 // Process Nunjucks
 function nunjucks(cb) {
-    gulp.src("src/pages/*.html")
-        .pipe(
-            nunjucksRender({
-                path: ["src/templates/"] // String or Array
-            })
-        )
-        .pipe(gulp.dest("dist"));
-    cb();
+  gulp
+    .src('src/pages/*.html')
+    .pipe(
+      nunjucksRender({
+        path: ['src/templates/'], // String or Array
+      })
+    )
+    .pipe(gulp.dest('dist'));
+  cb();
 }
 
 function nunjucksMinify(cb) {
-    gulp.src("src/pages/*.html")
-        .pipe(
-            nunjucksRender({
-                path: ["src/templates/"] // String or Array
-            })
-        )
-        .pipe(
-            htmlmin({
-                collapseWhitespace: true
-            })
-        )
-        .pipe(gulp.dest("dist"));
-    cb();
+  gulp
+    .src('src/pages/*.html')
+    .pipe(
+      nunjucksRender({
+        path: ['src/templates/'], // String or Array
+      })
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest('dist'));
+  cb();
 }
 
 // Watch Files
 function watch_files() {
-    browserSync.init({
-        server: {
-            baseDir: "dist/"
-        }
-    });
-    gulp.watch("src/assets/sass/**/*.scss", css);
-    gulp.watch("src/assets/js/*.js", js).on("change", browserSync.reload);
-    gulp.watch("src/pages/*.html", nunjucks).on("change", browserSync.reload);
-    gulp.watch("src/templates/*.html", nunjucks).on(
-        "change",
-        browserSync.reload
-    );
+  browserSync.init({
+    server: {
+      baseDir: 'dist/',
+    },
+  });
+  gulp.watch('src/assets/sass/**/*.scss', css);
+  gulp.watch('src/assets/js/*.js', js).on('change', browserSync.reload);
+  gulp.watch('src/pages/*.html', nunjucks).on('change', browserSync.reload);
+  gulp.watch('src/templates/*.html', nunjucks).on('change', browserSync.reload);
 }
 
 // Default 'gulp' command with start local server and watch files for changes.
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,41 @@
-console.log('i am working bro')
+const cta = document.querySelector('.left__intro-cta');
+
+const headingSpan = document.querySelectorAll('.left__intro__heading > span');
+
+const secondaryHeadingSpan = document.querySelector( '.left__intro__secondary-heading > span' ); // prettier-ignore
+
+const ctaBefore = document.querySelector('.left__intro-cta:hover::before');
+
+const header = document.querySelector('header .left');
+
+const rightHandSide = document.querySelector('.right');
+
+cta.addEventListener('mouseenter', function (e) {
+  cta.classList.add('left__intro-cta-hover');
+  rightHandSide.classList.add('right-hover');
+  secondaryHeadingSpan.classList.add('drug-color');
+  document.body.classList.add('drug-background');
+  header.classList.add('drug-background');
+
+  for (let i = 0; i < headingSpan.length; i++) {
+    headingSpan[i].classList.add('drug-color');
+    headingSpan[i].style.fontWeight = 'bold';
+  }
+});
+
+cta.addEventListener('mouseleave', function (e) {
+  cta.classList.remove('left__intro-cta-hover');
+  rightHandSide.classList.remove('right-hover');
+  secondaryHeadingSpan.style.color = '#F2F5FF';
+  secondaryHeadingSpan.classList.remove('drug-color');
+  document.body.classList.remove('drug-background');
+  header.classList.remove('drug-background');
+
+  console.log(headingSpan);
+
+  for (let i = 0; i < headingSpan.length; i++) {
+    headingSpan[i].style.color = '#F2F5FF';
+    headingSpan[i].style.fontWeight = 'normal';
+    headingSpan[i].classList.remove('drug-color');
+  }
+});
